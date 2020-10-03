4dc4e43f6cc923e8c55515a89ab8a75f3cac44cb
Artur <korbut.arturio@gmail.com>
2020-10-03 17:52:23 +0300

    fix:add test for minArray function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 exports.min = function min(array) {
-    var minArr = array[0];
-
     if (array && array.length > 0) {
+        var minArr = array[0];
         for (var i = 0; i < array.length; i++) {
             if (array[i] < minArr) {
                 minArr = array[i];
