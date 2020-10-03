82243c05178e379179ba412233a1f534916ff1b0
Artur <korbut.arturio@gmail.com>
2020-10-03 17:48:09 +0300

    fix: add test for averageArray function and maxArray function

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
 exports.min = function min(array) {
     var minArr = array[0];
 
-    if (array.length == 0) {
-        var result = 0;
-        return result;
-    } else {
+    if (array && array.length > 0) {
         for (var i = 0; i < array.length; i++) {
             if (array[i] < minArr) {
                 minArr = array[i];
             }
         }
         return minArr;
+    } else {
+        return 0;
     }
 };
 
 exports.max = function max(array) {
-    maxArr = array[0];
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] > maxArr) {
-            maxArr = array[i];
+    if (array && array.length > 0) {
+        maxArr = array[0];
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] > maxArr) {
+                maxArr = array[i];
+            }
         }
+        return maxArr;
+    } else {
+        return 0;
     }
-    return maxArr;
 };
 
 exports.avg = function avg(array) {
-    var sum = 0;
-    for (var i = 0; i < array.length; i++) {
-        sum += array[i];
+    if (array && array.length > 0) {
+        var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        var result = sum / array.length;
+        return result;
+    } else {
+        return 0;
     }
-    var result = sum / array.length;
-    return result;
 };
